39e32ec4259da39351cfd0743d54fa6b427fd6f5
n.teguri <n.teguri@wakuto.net>
2018-11-06 22:15:09 +0900

    Now can edit from browser

--- apps/public/js/main.js
+++ apps/public/js/main.js
@@ -1,4 +1,21 @@
 $(function() {
+  $('.edit').on('click', function() {
+    var target = $(this).prev();
+    target.removeAttr('readonly');
+    target.focus().blur(function() {
+      $(this).attr('readonly', true);
+      var id = $(this).attr('data-edit-id')
+      var content = $(this).val()
+      $.ajax({
+        url: '/todo/' + id,
+        type: 'PUT',
+        data: { content: content }
+      }).always(() => {
+        window.location.href = '/todo'
+      })
+    });
+  })
+
   $('.delete').on('click', function() {
     var id = $(this).attr('data-delete-id')
     $.ajax({
